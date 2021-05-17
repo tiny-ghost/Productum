0507308bd5d410905c8e3dfde39c8136b920ea48
Albert Apsit <albert.apsit@ericsson.com>
2021-05-18 00:10:08 +0300

    File restructure, viewmodel in app testing

---
+++ Productum.Desktop.UI/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Productum.Desktop.UI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productum.Desktop.UI.MVVM.ViewModel
+{
+    class MainWindowViewModel: ObservableObject
+    {
+        private string _title = "Productum - Home";
+
+        public string Title
+        {
+            get => _title; 
+            set => Set(ref _title, value);
+        }
+
+    }
+}
