668173c910aa419bdb7f658fef7dadbfb2f73dea
Albert Apsit <albert.apsit@ericsson.com>
2021-05-17 23:22:27 +0300

    ObservableObject patterns implemented, project restructure

--- Productum.Desktop.UI/Core/ObservableObject.cs
+++ Productum.Desktop.UI/Core/ObservableObject.cs
@@ -3,13 +3,25 @@ using System.Runtime.CompilerServices;
 
 namespace Productum.Desktop.UI.Core
 {
-    class ObservableObject : INotifyPropertyChanged
+    internal abstract class ObservableObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(name));
+        }
+
+        protected virtual bool Set<T> (ref T field, T value, [CallerMemberName] string name =null)
+        {
+            if(Equals(field,value))
+            {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged(name);
+            return true;
         }
     }
 }
